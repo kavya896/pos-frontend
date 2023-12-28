42411ccffce3e69a9f4793c6897be68e75fbc1e5
kavya896 <kavyareddyk1@gmail.com>
2023-12-28 18:36:08 +0530

    pos

--- src/components/Pos/Pos.jsx
+++ src/components/Pos/Pos.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import "./Pos.css"
+import { Typography } from "@mui/material";
 const Pos = () =>{
     return(
         <div className="pos">
-            Pos
+            <div>
+                <div className="headers">
+                <div>
+                    <div>8</div>
+                    <div>9</div>
+                </div>
+                </div>
+                <div className="tables">
+                   
+                        <div className="running-orders">
+                            <div>
+                                <Typography variant="h5">Running Orders</Typography>
+                            </div>
+                        </div>
+                        <div className="takeout">2</div>
+                        <div className="categories">3</div>
+                   
+                </div>
+            </div>
         </div>
     )
 }
